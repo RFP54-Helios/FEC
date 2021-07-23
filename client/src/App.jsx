6377510942d83e259f0de1003c3ffb571e59512c
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-07-23 10:57:52 -0700

    Configured Webpack to build React app components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,3 +4,13 @@
     //Related products
     //Q&A
     //Reviews and ratings
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let App = (props) => {
+  return (
+    <div>Hello from React</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));

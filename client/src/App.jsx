a2205c093c18051f5758d71ad2a57ca7de000700
Darion Williams <darionjwilliams@gmail.com>
2021-07-23 11:58:43 -0700

    Created and rendered components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,20 @@
     //Reviews and ratings
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Overview from './components/Overview.jsx';
+import Outfits from './components/Outfits.jsx';
+import QandA from './components/QandA.jsx';
+import Ratings from './components/Ratings.jsx';
 
 let App = (props) => {
   return (
-    <div>Hello from React</div>
+    <div>
+      <h2>FEC</h2>
+      <Overview />
+      <Outfits />
+      <QandA />
+      <Ratings />
+    </div>
   )
 }
 
---
+++ client/src/components/Ratings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Ratings = (props) => {
+  return(
+    <div>Ratings</div>
+  )
+}
+
+export default Ratings;

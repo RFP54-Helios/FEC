310c420e08927f8ca6749660094c3f8e5fa40d9c
Darion Williams <darionjwilliams@gmail.com>
2021-07-27 11:10:19 -0700

    Fixed style concerns, and reinstalled webpack.

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import Gallery from "./gallery/Gallery.jsx";
-import ProductInfo from "./ProductInfo.jsx";
-import StyleSelector from "./StyleSelector.jsx";
-import AddToCart from "./AddToCart.jsx";
+import React, { useState } from 'react';
+import Gallery from './gallery/Gallery.jsx';
+import ProductInfo from './ProductInfo.jsx';
+import StyleSelector from './StyleSelector.jsx';
+import AddToCart from './AddToCart.jsx';
 require('dotenv').config();
 
 const Overview = () => {
   return (
-    <div id="overview-components">
-      <Gallery id="gallery" />
-      <ProductInfo id="info" />
-      <StyleSelector id="styles" />
-      <AddToCart id="cart" />
+    <div id='overview-components'>
+      <Gallery id='gallery' />
+      <ProductInfo id='info' />
+      <StyleSelector id='styles' />
+      <AddToCart id='cart' />
     </div>
   );
 };

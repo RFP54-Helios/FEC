b02137bfe9a0c85235da373ffba9fc81a5138bed
Darion Williams <darionjwilliams@gmail.com>
2021-07-29 20:02:24 -0700

    Added generic render and state change tests

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -4,8 +4,24 @@ import '@testing-library/jest-dom';
 import App from './App.jsx';
 
 describe('State', () => {
+  afterEach(cleanup);
+
   it('Should render', () => {
     const { getByText } = render(<App />);
     expect(getByText(/FEC/i)).toBeInTheDocument();
+  })
+
+  it('Should update state when a related item is clicked', () => {
+    const {getByText} = render(<App />)
+
+    expect(getByText(/17069/)).toBeInTheDocument();
+    fireEvent.click(getByText(/17069/));
+    expect(getByText(/17070/)).toBeInTheDocument();
+  })
+
+  it('Should display the correct name of the current product', () => {
+    const {getByText} = render(<App />);
+
+    expect(getByText(/Morning Joggers/i)).toBeInTheDocument();
   })
 })

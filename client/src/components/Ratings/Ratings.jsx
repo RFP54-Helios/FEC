1c54b21b37ceeb6341ccdc7bb1336a0372d8af7f
Matt Heindel <mheindel94@gmail.com>
2021-07-30 14:01:05 -0700

    Store API responses in component state, base off App state
    `useEffect()` refreshes component state when the App level `product_id`
    is modified.

--- client/src/components/Ratings/Ratings.jsx
+++ client/src/components/Ratings/Ratings.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import Stars from './Stars.jsx';
 import Reviews from './Reviews.jsx';
-import sampleReviews from './sample-reviews.json';
-import sampleMeta from './sample-meta.json';
+import { ProductContext } from '../../App.jsx';
+
+import { getFromApi } from '../../helperFunctions.js';
 
 const Ratings = (props) => {
+
+  // App level state
+  const [product, setProduct] = useContext(ProductContext);
+
+  // component level state
+  const [ratings, setRatings] = useState({
+    sort: 'relevant',
+    reviews: [],
+    meta: {}
+  });
+
+  // run on page load
+  useEffect(() => {
+    // store reviews in state
+    Promise.all([
+      getFromApi('reviews', {
+        product_id: product.product_id,
+        count: 6,
+        sort: ratings.sort
+      }),
+      getFromApi(`reviews/meta?product_id=${product.product_id}`)
+    ])
+    .then(ratingResults => {
+      setRatings(prevState => ({
+        ...prevState,
+        reviews: ratingResults[0].results,
+        meta: ratingResults[1]
+      }));
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  });
+  // listen for changes
+  // useEffect(() => { }, [product.product_id])
+
   return (
     <div id='ratings-container'>
       <div id='ratings-container-left'>
@@ -13,8 +50,8 @@ const Ratings = (props) => {
       </div>
       <div id='ratings-container-right'>
         <Reviews
-          reviews={ sampleReviews.results }
-          meta={ sampleMeta }
+          reviews={ ratings.reviews }
+          meta={ ratings.meta }
         />
       </div>
     </div>

a0702564fdea374e9bb67a75ffc3806d3877957c
Matt Heindel <mheindel94@gmail.com>
2021-07-31 12:15:30 -0700

    Refactor `Overview` to use dynamic ratings in `Stars` component

--- client/src/components/overview/Panel/Rating.jsx
+++ client/src/components/overview/Panel/Rating.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Stars from '../../../Stars.jsx';
 
-const Rating = () => {
+const Rating = (props) => {
   return(
     <div id='rating'>
-      <p>★★★★★</p>
+      <Stars ratings={props.ratings} />
       <a href=''>Read all reviews</a>
     </div>
   )
+}
+Rating.propTypes = {
+  ratings: PropTypes.array.isRequired,
 }
 
 export default Rating;

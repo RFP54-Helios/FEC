a8aa8b775c13365a1a6fc8fa7b29819cfc113c00
Matt Heindel <mheindel94@gmail.com>
2021-07-30 17:22:07 -0700

    Add dependency for icons

--- client/src/Stars.jsx
+++ client/src/Stars.jsx
@@ -2,17 +2,21 @@ import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 import { ProductContext } from "./App.jsx";
 
+import { BsStar } from 'react-icons/bs';
+import { BsStarFill } from 'react-icons/bs';
+import { BsStarHalf } from 'react-icons/bs';
+
 const Stars = (props) => {
   const [product, setProduct] = useContext(ProductContext);
 
-  return(
+  return (
     <div className='ratings-stars'>
       <p>{props.starsCount}</p>
-      <p className='ratings-star'>*</p>
-      <p className='ratings-star'>*</p>
-      <p className='ratings-star'>*</p>
-      <p className='ratings-star'>*</p>
-      <p className='ratings-star'>*</p>
+      <BsStarFill />
+      <BsStarHalf />
+      <BsStar />
+      <BsStar />
+      <BsStar />
     </div>
   );
 }

cf2d57d385007de79aaff6a9bc2da0f91c37b619
Darion Williams <darionjwilliams@gmail.com>
2021-07-31 20:11:12 -0700

    useEffect was inconsistently with re-renders. Refactored loading bool back to direct ternary expression for conditional render.

--- client/src/components/overview/Description/DescriptionText.jsx
+++ client/src/components/overview/Description/DescriptionText.jsx
@@ -3,21 +3,18 @@ import { ProductContext } from "../../../App.jsx";
 
 const DescriptionText = () => {
   const [product, setProduct] = useContext(ProductContext)
-  const [isLoading, setLoadingStatus] = useState(true)
-
-  useEffect(() => {
-    if(product.currentProduct.slogan) setLoadingStatus(false);
-  })
 
   return (
     <div id='description-text'>
-      {isLoading ?
-        <h3>{product.currentProduct.slogan}</h3> :
-        <h3>Thinking of a slogan...</h3>
-      }
-      {isLoading ?
-        <p>{product.currentProduct.description}</p> :
-        <p>Fetching some juicy details...</p>
+      {product.currentProduct.slogan
+        ? <>
+            <h3>{product.currentProduct.slogan}</h3><br></br>
+            <p>{product.currentProduct.description}</p>
+          </>
+        : <>
+            <h3>Thinking of a slogan...</h3><br></br>
+            <p>Fetching some juicy details...</p>
+          </>
       }
     </div>
   )

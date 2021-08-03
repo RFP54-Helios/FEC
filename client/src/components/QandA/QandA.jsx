ceb7789a5b6c6beb703c124358eff86686b37450
Emily Liu <emilylau211@gmail.com>
2021-08-03 15:36:21 -0700

    Implemented adding 2 questions each time on click

--- client/src/components/QandA/QandA.jsx
+++ client/src/components/QandA/QandA.jsx
@@ -11,28 +11,36 @@ const QandA = (props) => {
   const [seeMoreQuestions, setSeeMoreQuestions] = useState(false);
   const toggleMoreQuestions = () => setSeeMoreQuestions(!seeMoreQuestions);
   const [addQuestionPopup, setAddQuestionPopup] = useState(false);
+  const [counter, setCounter] = useState(4);
 
   let moreQuestionsBtn;
+
+  let searchResult = questionData.slice(0, counter);
+  const increaseByTwo = () => {
+    setCounter(counter + 2);
+    if(counter >= questionData.length) { toggleMoreQuestions(); }
+  };
+
+
+
   if (questionData.length > 4) {
     moreQuestionsBtn =
-    <div><button className='see-more-questions' onClick={() => {toggleMoreQuestions();}}>
+    <div><button className='see-more-questions' onClick={() => {increaseByTwo();}}>
     More Answered Questions</button></div>
   }
   if (seeMoreQuestions===true) {
     moreQuestionsBtn = null;
   }
 
-  let searchResult;
-  if (searching.length < 3) {
-    searchResult = questionData;
-  } else {
+  if (searching.length >= 3) {
     moreQuestionsBtn = null;
     searchResult = questionData.filter((question) => {
       const questionContent = question.question_body.toLowerCase();
       return questionContent.includes(searching);
     });
   }
-  let moreQuestions = searchResult.slice(4);
+
+
   // let sortedQuestionList;
   // sortedQuestionList= Object.values(questionData).sort(function(a, b) {
   //   return b.question_helpfulness - a.question_helpfulness;
@@ -53,37 +61,15 @@ const QandA = (props) => {
       <h3>Customer questions & answers</h3>
       <input className='search' type='text' value={searching} placeholder='Have a question? Search for answers…' onChange={() => setSearching(event.target.value)} />
       <div id='body'>
-      {searchResult.slice(0, 4).map(question =>
+      {searchResult.map(question =>
         <QuestionList question={question} />
       )}
       {moreQuestionsBtn}
-      {seeMoreQuestions===true ? <ViewMoreQuestions questions={moreQuestions} /> : null}
       </div>
       <button className='ask-question-btn' onClick={(e) => {e.preventDefault(); setAddQuestionPopup(true)}}>Ask your question</button>
       <AddQuestion trigger={addQuestionPopup} setTrigger={setAddQuestionPopup}>
       </AddQuestion>
     </div>
-  )
-};
-
-
-const ViewMoreQuestions = (props) => {
-  const [evenMore, setEvenMore] = useState(false);
-  const toggleEvenMore = ()=> {setEvenMore(!evenMore)};
-  const [moreQBtn, setMoreQBtn] = useState(false);
-  const toggleMoreQBtn = () => { setMoreQBtn(!moreQBtn) };
-  let twoQuestions = props.questions.splice(0, 2);
-
-  return (
-    <>
-    <div>
-    {props.questions.map(question =>
-      <QuestionList question={question}/>
-    )}
-    </div>
-    {moreQBtn ? <button onClick={() => { toggleMoreQBtn(); }}>See more questions</button> : null}
-    {/* {evenMore ? <EvenMoreQ questions={props.questions} /> : null} */}
-    </>
   )
 };
 

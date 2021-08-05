29e91cf43d3b24e794a76af6f437cc943fee4fcf
Darion <79764148+darionbuilds@users.noreply.github.com>
2021-08-04 19:31:11 -0700

    Added comments to new Thumbnails functionality

--- client/src/components/overview/gallery/Thumbnails.jsx
+++ client/src/components/overview/gallery/Thumbnails.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 
 const Thumbnails = ({ index, thumbnail, galleryImageIndex }) => {
+  // logic to work around the map index starting from 0 on new thumbnail sets
+  // since each set is a new array, map begins counting at 0 despite being index 7, etc
   const isMatchingThumbnail = () => {
+    // if this is the first set, the focused thumbnail is accurate
     if (galleryImageIndex <= 6) {
       return index === galleryImageIndex;
+      // if this is the second set, we need to offset the index by -7
     } else if (galleryImageIndex >= 7 && galleryImageIndex <= 13) {
       return index === galleryImageIndex - 7;
+      // same, but 14 for this case. > 21 thumbnails is unlikely
     } else {
       return index === galleryImageIndex - 14;
     }

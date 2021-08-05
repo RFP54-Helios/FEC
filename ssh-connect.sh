d06014be1fd90279b3036f63dd47b5ab4fb76ea9
Matt Heindel <mheindel94@gmail.com>
2021-08-05 13:28:55 -0700

    Optimize `npm run build` to use webpack production mode

---
+++ ssh-connect.sh
@@ -0,0 +1,2 @@
+ssh -i helios.pem ubuntu@3.15.166.249
+
---
+++ webpack.prod.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const OUT_DIR = path.join(__dirname, 'client', 'dist');
+
+module.exports = {
+  entry: path.join(SRC_DIR, 'index.js'),
+  output: {
+    path: OUT_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test:/\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  mode: 'production',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
